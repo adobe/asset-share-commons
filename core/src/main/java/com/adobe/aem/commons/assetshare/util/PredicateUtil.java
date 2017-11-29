915be539c09d0d0403634689bfc9664e8cec5ff9
david g <davidjgonzalez@gmail.com>
2017-11-29 09:03:04 -0500

    #75 - Fixed javadoc issues

--- core/src/main/java/com/adobe/aem/commons/assetshare/util/PredicateUtil.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/util/PredicateUtil.java
@@ -36,7 +36,7 @@ public final class PredicateUtil {
      *
      * @param request the request object.
      * @param parameterName The request query parameter name.
-     * @return a String representation of the query parameter {@param parameterName}. If no request parameter can be found with that name, the empty string is returned.
+     * @return a String representation of the query parameter parameterName. If no request parameter can be found with that name, the empty string is returned.
      */
     public static String getParamFromQueryParams(final SlingHttpServletRequest request, final String parameterName) {
         final RequestParameter requestParameter = request.getRequestParameter(parameterName);
@@ -49,24 +49,24 @@ public final class PredicateUtil {
     }
 
     /**
-     * Determines if the {@optionItem}'s value exists as a value in the {@initialValues} map.
+     * Determines if the optionItem's value exists as a value in the initialValues map.
      *
      * @param optionItem the option item.
      * @param initialValues the initial values.
-     * @return true if {@param optionItem}'s value is in {@initialValues}.
+     * @return true if optionItem's value is in initialValues.
      */
     public static boolean isOptionInInitialValues(OptionItem optionItem, ValueMap initialValues) {
         return isOptionInInitialValues(optionItem.getValue(), initialValues);
     }
 
     /**
-     * Determines if the {@param value} exists as a value in the initialValues map.
+     * Determines if the value exists as a value in the initialValues map.
      *
      * Only String and String[] intialValues values are supported.
      *
      * @param value the value to check.
      * @param initialValues the initial values.
-     * @return true if {@param value} is in {@initialValues}.
+     * @return true if value is in initialValues.
      */
     public static boolean isOptionInInitialValues(String value, ValueMap initialValues) {
         for (final String key : initialValues.keySet()) {
@@ -79,7 +79,7 @@ public final class PredicateUtil {
 
     /**
      * @param needle the value to check for.
-     * @param haystack they values to check if the {@param needle} is in (must be String or String[]).
+     * @param haystack they values to check if the needle is in (must be String or String[]).
      * @return true is the needle exists in the haystack.
      */
     private static boolean isValueInValueMap(String needle, Object haystack) {
