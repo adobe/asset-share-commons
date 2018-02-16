699f2dc7f5746a0f5e938f73ab9e1cf904080970
david g <davidjgonzalez@gmail.com>
2018-02-15 22:37:18 -0500

    #134 - Multiple ShareService support.

--- core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/share/impl/ShareServlet.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/share/impl/ShareServlet.java
@@ -19,6 +19,7 @@
 
 package com.adobe.aem.commons.assetshare.components.actions.share.impl;
 
+import com.adobe.aem.commons.assetshare.components.actions.share.Share;
 import com.adobe.aem.commons.assetshare.components.actions.share.ShareException;
 import com.adobe.aem.commons.assetshare.components.actions.share.ShareService;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -28,6 +29,7 @@ import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,20 +37,26 @@ import javax.jcr.RepositoryException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
 
-@Component(service = Servlet.class, property = {
-        "sling.servlet.methods=POST",
-        "sling.servlet.resourceTypes=asset-share-commons/actions/share",
-        "sling.servlet.selectors=share",
-        "sling.servlet.extensions=html",
-        "sling.servlet.extensions=json"
-})
+@Component(service = Servlet.class,
+           property = {
+            "sling.servlet.methods=POST",
+            "sling.servlet.resourceTypes=asset-share-commons/actions/share",
+            "sling.servlet.selectors=share",
+            "sling.servlet.extensions=html",
+            "sling.servlet.extensions=json"
+            }
+)
 public class ShareServlet extends SlingAllMethodsServlet {
     private static final Logger log = LoggerFactory.getLogger(ShareServlet.class);
 
-    @Reference
-    private ShareService shareService;
+    @Reference(target = "(component.name=com.adobe.aem.commons.assetshare.components.actions.share.impl.EmailShareServiceImpl)")
+    private ShareService defaultShareService;
+
+    @Reference(policyOption = ReferencePolicyOption.GREEDY)
+    private transient Collection<ShareService> shareServices;
 
     @Override
     protected final void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response)
@@ -62,13 +70,20 @@ public class ShareServlet extends SlingAllMethodsServlet {
 
     private final void share(SlingHttpServletRequest request, SlingHttpServletResponse response) throws RepositoryException {
         try {
-            if (shareService != null) {
-                // Make this map write-able by copying the Request parameters into a new hash map
-                final ValueMap shareParameters = new ValueMapDecorator(new HashMap<String, Object>(request.getParameterMap()));
-                // Call the best ranking Share Service
-                shareService.share(request, response, shareParameters);
-            } else {
-                throw new ShareException("No ShareService is active, so sharing is disabled");
+            // Make this map write-able by copying the Request parameters into a new hash map
+            final ValueMap shareParameters = new ValueMapDecorator(new HashMap<String, Object>(request.getParameterMap()));
+
+            int shareCount = 0;
+            // Call the best ranking Share Service
+            for(final ShareService shareService : shareServices) {
+                if (shareService.accepts(request)) {
+                    shareService.share(request, response, shareParameters);
+                    shareCount++;
+                }
+            }
+
+            if (shareCount == 0) {
+                defaultShareService.share(request, response, shareParameters);
             }
         } catch (ShareException ex) {
             log.error("Unable to share assets from Asset Share Commons", ex);
