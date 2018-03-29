5bd19b677bc8adb4ed2f1fc63f7640957579f41c
Mahendra R <rmahendr@adobe.com>
2018-03-29 17:14:04 +0530

    Dynamic Media download modal - intial changes

---
+++ core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/dmdownload/impl/DMDownloadImpl.java
@@ -0,0 +1,147 @@
+/*
+ * Asset Share Commons
+ *
+ * Copyright (C) 2017 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.adobe.aem.commons.assetshare.components.actions.dmdownload.impl;
+
+import com.adobe.aem.commons.assetshare.components.actions.ActionHelper;
+import com.adobe.aem.commons.assetshare.components.actions.dmdownload.DMDownload;
+import com.adobe.aem.commons.assetshare.content.AssetModel;
+import com.day.cq.dam.entitlement.api.EntitlementConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.featureflags.Features;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.Required;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.tenant.Tenant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+@Model(
+        adaptables = {SlingHttpServletRequest.class},
+        adapters = {DMDownload.class},
+        resourceType = {DMDownloadImpl.RESOURCE_TYPE},
+        cache = true
+)
+public class DMDownloadImpl implements DMDownload {
+    protected static final String RESOURCE_TYPE = "asset-share-commons/components/modals/download";
+
+    @Self
+    @Required
+    private SlingHttpServletRequest request;
+
+    @ValueMapValue
+    @Optional
+    @Default(values = "Assets")
+    private String zipFileName;
+
+    @OSGiService
+    @Required
+    private ActionHelper actionHelper;
+
+    @OSGiService
+    @Required
+    private Features features;
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private final String SCENE7_FEATURE_FLAG = "com.adobe.dam.asset.scene7.feature.flag";
+    private final String IAMGE_PRESET_PATH_DEFAULT = "/macros";
+    private final String IMAGE_SERVER_PATH_ROOT = "/etc/dam/imageserver";
+    private final String IAMGE_PRESET_PATH_DEFAULT_CONF = "/conf/global/settings/dam/dm/presets";
+
+    private final Logger log = LoggerFactory.getLogger(DMDownload.class);
+
+    private Collection<AssetModel> assets = new ArrayList<>();
+
+    private boolean isDynamicMediaEnabled;
+
+    private Collection<String> imagePresets = new ArrayList<>();
+
+    @PostConstruct
+    protected void init() {
+        if(features == null) {
+            isDynamicMediaEnabled = false;
+            return;
+        }
+
+        if(features.isEnabled(EntitlementConstants.ASSETS_DYNAMICMEDIA_FEATURE_FLAG_PID) ||
+                features.isEnabled(SCENE7_FEATURE_FLAG)) {
+            isDynamicMediaEnabled = true;
+            setImagePresets();
+
+            log.info("Presets {}", imagePresets.toString());
+        }
+
+        assets = actionHelper.getAssetsFromQueryParameter(request, "path");
+
+        if (assets.isEmpty()) {
+            assets = actionHelper.getPlaceholderAsset(request);
+        }
+    }
+
+    public Collection<AssetModel> getAssets() {
+        return assets;
+    }
+
+    public String getZipFileName() {
+        return StringUtils.removeEndIgnoreCase(zipFileName, ".zip");
+    }
+
+    public boolean isDynamicMediaEnabled() { return isDynamicMediaEnabled; }
+
+    public Collection<String> getImagePresets() {
+        return imagePresets;
+    }
+
+    private void setImagePresets() {
+        Tenant tenant = resourceResolver.adaptTo(Tenant.class);
+        String tenantId = null;
+        String imagePresetPath = IMAGE_SERVER_PATH_ROOT;
+        if (tenant != null && tenant.getId() != null) {
+            imagePresetPath += "/tenants/" + tenant.getId() + IAMGE_PRESET_PATH_DEFAULT;
+        } else {
+            imagePresetPath += IAMGE_PRESET_PATH_DEFAULT;
+        }
+
+        Resource imgPresetResource = resourceResolver.getResource(imagePresetPath);
+        if (imgPresetResource != null) {
+            Iterator<Resource> imgPresetIterator = imgPresetResource.listChildren();
+            while (imgPresetIterator.hasNext()) {
+                Resource imgPreset = imgPresetIterator.next();
+                if (imgPreset != null){
+                    imagePresets.add(imgPreset.getName());
+                }
+            }
+        }
+    }
+}
