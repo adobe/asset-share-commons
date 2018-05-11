3a51d11164d1e811fec8f9ef86ba3f06c55a698e
kanika <kakapoor@adobe.com>
2018-05-11 19:54:20 +0530

    [feature/185] - refactored JS and added copyright to all files

--- ui.apps/src/main/content/jcr_root/apps/asset-share-commons/clientlibs/clientlib-site/js/content-fragment/content-fragment.js
+++ ui.apps/src/main/content/jcr_root/apps/asset-share-commons/clientlibs/clientlib-site/js/content-fragment/content-fragment.js
@@ -1,26 +1,52 @@
-    AssetShare.ContentFragment = (function ($, window, ns) {
+/*
+ * Asset Share Commons
+ *
+ * Copyright [2017]  Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*global jQuery: false, AssetShare: false, window: false */
+
+AssetShare.ContentFragment = (function ($, window, ns) {
+    "use strict";
 
     function setHtml(elem, htmlStr, loader) {
-        if(loader)
-        	loader.hide();
-        if(elem)
-        	elem.html(htmlStr);
+        if (loader) {
+            loader.hide();
+        }
+        if (elem) {
+            elem.html(htmlStr);
+        }
     }
 
     function setCFVariation(url, contentFragmentContainer, fallbackHeader, fallbackMsg, placeholderImgContainer, loader) {
-        $.get(url, function(result) {
+        $.get(url, function (result) {
             if (result.length === 0) {
                 setHtml(fallbackHeader, fallbackMsg, loader);
             } else {
                 setHtml(contentFragmentContainer, result, loader);
             }
-        }).fail(function() {
-            if (loader)
-            	loader.hide();
-			if(fallbackHeader)
-            	fallbackHeader.hide();
-            if(placeholderImgContainer)
-            	placeholderImgContainer.show();
+        }).fail(function () {
+            if (loader) {
+                loader.hide();
+            }
+            if (fallbackHeader) {
+                fallbackHeader.hide();
+            }
+            if (placeholderImgContainer) {
+                placeholderImgContainer.show();
+            }
         });
     }
 
@@ -31,7 +57,7 @@
             pathList = window.location.pathname.split(regex),
             queryString = window.location.search.replace("?", ''),
             queryParams = queryString.split("&"),
-            variationParam = queryParams.filter(function(param) { return param.includes("variation="); });
+            variationParam = queryParams.filter(function (param) { return param.includes("variation="); });
 
         if (pathList.length === 3) {
             originalVarSelPath = pathList[2] + ".cfm.content.html";
@@ -59,6 +85,4 @@
         setCFVariation: setCFVariation
     };
 
-    }(jQuery, window, AssetShare));
-
-
+}(jQuery, window, AssetShare));
--- ui.apps/src/main/content/jcr_root/apps/asset-share-commons/components/details/content-fragment/cf-variations/clientlibs/site/js/cf-variations.js
+++ ui.apps/src/main/content/jcr_root/apps/asset-share-commons/components/details/content-fragment/cf-variations/clientlibs/site/js/cf-variations.js
@@ -1,4 +1,25 @@
+/*
+ * Asset Share Commons
+ *
+ * Copyright [2017]  Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*global jQuery: false, AssetShare: false, window: false */
+
 jQuery((function ($, window, ns) {
+    "use strict";
     (function () {
 
         var variationButton =$(".cmp-details-cf-variations .cf-variation-label"),
@@ -21,7 +42,7 @@ jQuery((function ($, window, ns) {
         function showVariant() {
             var url = ns.getCFSelectorBasedPath();
             if (url.length > 0) {
-                ns.setCFVariation(url,contentFragmentContainer)
+                ns.setCFVariation(url,contentFragmentContainer);
             }
         }
 
