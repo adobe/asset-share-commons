f4b8103d4496c04d72016e3d5b9d16dbd4b5604e
kaushalmall <kaushalmall@gmail.com>
2018-09-25 13:02:21 -0700

    Feature/send 404 #283 (#289)
    * AssetDetails404Servlet now `response.sendsError(404)` so that ACS Commons Error Page Handler/Sling error handler can handle this as a 404.

--- core/src/main/java/com/adobe/aem/commons/assetshare/configuration/impl/AssetDetails404Servlet.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/configuration/impl/AssetDetails404Servlet.java
@@ -51,10 +51,8 @@ public class AssetDetails404Servlet extends SlingSafeMethodsServlet implements O
     protected final void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
             throws ServletException, IOException {
         // Control which requests enter this method via accepts(..) below.
-
-        response.setStatus(SlingHttpServletResponse.SC_NOT_FOUND);
-        response.getWriter().print("The path to a valid and accessible asset must be provided to the Asset Details page for it to display.");
-        response.getWriter().flush();
+        //send 404 error to let Sling handle it.
+        response.sendError(SlingHttpServletResponse.SC_NOT_FOUND);
     }
 
     @Override public boolean accepts(@Nonnull final SlingHttpServletRequest request) {
