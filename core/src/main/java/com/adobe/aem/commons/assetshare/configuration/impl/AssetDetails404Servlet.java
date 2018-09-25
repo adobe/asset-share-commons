26633d59bd1974bbb9f478428350aa513fc1e33c
Kaushal Mall <kaushalmall@gmail.com>
2018-09-25 12:59:19 -0700

    added comment.

--- core/src/main/java/com/adobe/aem/commons/assetshare/configuration/impl/AssetDetails404Servlet.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/configuration/impl/AssetDetails404Servlet.java
@@ -51,7 +51,7 @@ public class AssetDetails404Servlet extends SlingSafeMethodsServlet implements O
     protected final void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
             throws ServletException, IOException {
         // Control which requests enter this method via accepts(..) below.
-
+        //send 404 error to let Sling handle it.
         response.sendError(SlingHttpServletResponse.SC_NOT_FOUND);
     }
 
