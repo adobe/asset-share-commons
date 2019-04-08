334dde9eb876c05a729218510aa3241f2be004f2
david g <davidjgonzalez@gmail.com>
2019-04-08 08:22:30 -0400

    Using AssetRenditionsServlet

---
+++ core/src/main/java/com/adobe/aem/commons/assetshare/content/properties/impl/AssetRenditionImpl.java
@@ -0,0 +1,113 @@
+/*
+ * Asset Share Commons
+ *
+ * Copyright (C) 2019 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.adobe.aem.commons.assetshare.content.properties.impl;
+
+import com.adobe.aem.commons.assetshare.content.properties.AbstractComputedProperty;
+import com.adobe.aem.commons.assetshare.content.properties.ComputedProperty;
+import com.adobe.aem.commons.assetshare.util.MimeTypeHelper;
+import com.adobe.cq.commerce.common.ValueMapDecorator;
+import com.day.cq.dam.api.Asset;
+import com.day.cq.dam.api.Rendition;
+import com.day.cq.dam.commons.util.DamUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ValueMap;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+import java.util.HashMap;
+
+import static com.adobe.aem.commons.assetshare.content.properties.ComputedProperty.DEFAULT_ASC_COMPUTED_PROPERTY_SERVICE_RANKING;
+
+@Component(
+        service = ComputedProperty.class,
+        property = {
+                Constants.SERVICE_RANKING + "=" + DEFAULT_ASC_COMPUTED_PROPERTY_SERVICE_RANKING
+        }
+)
+@Designate(ocd = AssetRenditionImpl.Cfg.class)
+public class AssetRenditionImpl extends AbstractComputedProperty<String> {
+
+    public static final String LABEL = "Asset Rendition";
+    public static final String NAME = "rendition";
+
+    private Cfg cfg;
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getLabel() {
+        return cfg.label();
+    }
+
+    @Override
+    public String[] getTypes() {
+        return cfg.types();
+    }
+
+    @Override
+    public String get(Asset asset, SlingHttpServletRequest request) {
+        // Move to params after merge
+        ValueMap parameters = new ValueMapDecorator(new HashMap<>());
+
+        String url = asset.getPath() + ".renditions/" + parameters.get("name", String.class) + "/";
+
+        if (parameters.get("download", false)) {
+            url += "download/";
+        }
+
+        url += "asset.rendition";
+
+        return escapeString(url);
+    }
+
+    private String escapeString(String str) {
+        str = StringUtils.replace(str, " ", "%20");
+        return StringUtils.replace(str, "/jcr:content", "/_jcr_content");
+    }
+
+    @Activate
+    protected void activate(Cfg cfg) {
+        this.cfg = cfg;
+    }
+
+    @ObjectClassDefinition(name = "Asset Share Commons - Computed Property - Asset Rendition")
+    public @interface Cfg {
+        @AttributeDefinition(
+                name = "Label",
+                description = "Human read-able label."
+        )
+        String label() default LABEL;
+
+        @AttributeDefinition(
+                name = "Types",
+                description = "Defines the type of data this exposes. This classification allows for intelligent exposure of Computed Properties in DataSources, etc."
+        )
+        String[] types() default { };
+    }
+}
