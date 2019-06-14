5d9d337c7f3d712373c14ff32cb90ef644abd118
david g <davidjgonzalez@gmail.com>
2019-06-13 20:34:38 -0400

    #421 - AssetRenditions thumbnail sizes on AEM 6.3.x, and null input handling in UrlUtil.

--- core/src/test/java/com/adobe/aem/commons/assetshare/util/UrlUtilTest.java
+++ core/src/test/java/com/adobe/aem/commons/assetshare/util/UrlUtilTest.java
@@ -19,50 +19,48 @@
 
 package com.adobe.aem.commons.assetshare.util;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class UrlUtilTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
 
     @Test
     public void escape_WithDoubleEscaping() {
         // allow double escaping
-        assertEquals("/content/dam/test.png", UrlUtil.escape("/content/dam/test.png", false));
-        assertEquals("/content/dam/test%20asset.png", UrlUtil.escape("/content/dam/test asset.png", false));
-        assertEquals("/content/dam/test%20asset.png/_jcr_content", UrlUtil.escape("/content/dam/test asset.png/jcr:content", false));
-        assertEquals("/content/dam/test%20folder/ir%c4%81%2bpu%20p%c3%b6%20%26p%c3%aep%e2%98%83.jpeg/_jcr_content/test",
+        Assert.assertNull(UrlUtil.escape(null, false));
+
+        Assert.assertEquals("/content/dam/test.png", UrlUtil.escape("/content/dam/test.png", false));
+        Assert.assertEquals("/content/dam/test%20asset.png", UrlUtil.escape("/content/dam/test asset.png", false));
+        Assert.assertEquals("/content/dam/test%20asset.png/_jcr_content", UrlUtil.escape("/content/dam/test asset.png/jcr:content", false));
+        Assert.assertEquals("/content/dam/test%20folder/ir%c4%81%2bpu%20p%c3%b6%20%26p%c3%aep%e2%98%83.jpeg/_jcr_content/test",
                 UrlUtil.escape("/content/dam/test folder/irā+pu pö &pîp☃.jpeg/jcr:content/test", false));
     }
-
-
 
     @Test
     public void escape_WithNoDoubleEscaping() {
         // No double escaping
-        assertEquals("/content/dam/test.png", UrlUtil.escape("/content/dam/test.png"));
-        assertEquals("/content/dam/test%20asset.png", UrlUtil.escape("/content/dam/test asset.png"));
-        assertEquals("/content/dam/test%20asset.png/_jcr_content", UrlUtil.escape("/content/dam/test asset.png/jcr:content"));
-        assertEquals("/content/dam/test%20folder/ir%c4%81%2bpu%20p%c3%b6%20%26p%c3%aep%e2%98%83.jpeg/_jcr_content/test",
+        Assert.assertNull(UrlUtil.escape(null));
+
+        Assert.assertEquals("/content/dam/test.png", UrlUtil.escape("/content/dam/test.png"));
+        Assert.assertEquals("/content/dam/test%20asset.png", UrlUtil.escape("/content/dam/test asset.png"));
+        Assert.assertEquals("/content/dam/test%20asset.png/_jcr_content", UrlUtil.escape("/content/dam/test asset.png/jcr:content"));
+        Assert.assertEquals("/content/dam/test%20folder/ir%c4%81%2bpu%20p%c3%b6%20%26p%c3%aep%e2%98%83.jpeg/_jcr_content/test",
                 UrlUtil.escape("/content/dam/test folder/irā+pu pö &pîp☃.jpeg/jcr:content/test"));
     }
 
     @Test
     public void isEscaped() {
-        assertTrue(UrlUtil.isEscaped("/content/dam/test.png"));
-        assertTrue(UrlUtil.isEscaped("/content/dam/test%20asset.png"));
-        assertTrue(UrlUtil.isEscaped("/content/dam/test%20asset.png/_jcr_content"));
-        assertTrue(UrlUtil.isEscaped("/content/dam/testasset.png/_jcr_content"));
-        assertTrue(UrlUtil.isEscaped("/content/dam/test%20folder/ir%c4%81%2bpu%20p%c3%b6%20%26p%c3%aep%e2%98%83.jpeg/_jcr_content/test"));
+        Assert.assertFalse(UrlUtil.isEscaped(null));
 
-        assertFalse(UrlUtil.isEscaped("/content/dam/test asset.png"));
-        assertFalse(UrlUtil.isEscaped("/content/dam/test asset.png/jcr:content"));
-        assertFalse(UrlUtil.isEscaped("/content/dam/testasset.png/jcr:content"));
-        assertFalse(UrlUtil.isEscaped("/content/dam/test folder/irā+pu pö &pîp☃.jpeg/jcr:content/test"));
+        Assert.assertTrue(UrlUtil.isEscaped("/content/dam/test.png"));
+        Assert.assertTrue(UrlUtil.isEscaped("/content/dam/test%20asset.png"));
+        Assert.assertTrue(UrlUtil.isEscaped("/content/dam/test%20asset.png/_jcr_content"));
+        Assert.assertTrue(UrlUtil.isEscaped("/content/dam/testasset.png/_jcr_content"));
+        Assert.assertTrue(UrlUtil.isEscaped("/content/dam/test%20folder/ir%c4%81%2bpu%20p%c3%b6%20%26p%c3%aep%e2%98%83.jpeg/_jcr_content/test"));
+
+        Assert.assertFalse(UrlUtil.isEscaped("/content/dam/test asset.png"));
+        Assert.assertFalse(UrlUtil.isEscaped("/content/dam/test asset.png/jcr:content"));
+        Assert.assertFalse(UrlUtil.isEscaped("/content/dam/testasset.png/jcr:content"));
+        Assert.assertFalse(UrlUtil.isEscaped("/content/dam/test folder/irā+pu pö &pîp☃.jpeg/jcr:content/test"));
     }
 }
