62c751391bfb8b7dfe4c66ea85c236ab2f0309f8
Daniel Gordon <dgordon@adobe.com>
2020-01-24 15:13:01 -0800

    initial refactor to move semantic ui light into own project

---
+++ ui.frontend.light/clientlib.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/asset-share-commons/clientlibs/clientlib-theme",
+
+    libs: {
+        name: "semanticui-light",
+        allowProxy: true,
+        categories: ["asset-share-commons.semantic-ui-light"],
+        embed: ["asset-share-commons.site.semantic-ui","asset-share-commons.site.semantic-ui.components"],
+        serializationFormat: "xml",
+        cssProcessor : ["default:none", "min:none"],
+        jsProcessor: ["default:none", "min:none"],
+        assets: {
+            js: [
+                "dist/semanticui-light/semantic.min.js"
+            ],
+            css: [
+                "dist/semanticui-light/semantic.min.css"
+            ],
+            resources: [
+                {
+                    src: "dist/themes/default/assets/fonts/*",
+                    dest: "assets/fonts"
+                },
+                {
+                    src: "dist/themes/light/assets/fonts/*",
+                    dest: "assets/fonts"
+                }
+            ]
+        }
+    }
+};
