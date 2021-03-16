05a6cc175d956b70d92f8dc945a9bb39c9f9fdef
Daniel Gordon <dgordon@adobe.com>
2021-03-16 13:48:13 -0700

    added license files

--- ui.apps/src/main/content/jcr_root/apps/asset-share-commons/clientlibs/clientlib-site/js/profile.js
+++ ui.apps/src/main/content/jcr_root/apps/asset-share-commons/clientlibs/clientlib-site/js/profile.js
@@ -1,3 +1,21 @@
+/*
+ * Asset Share Commons
+ *
+ * Copyright [2020]  Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Utility specific to storing data about the users Profile
  */
@@ -15,13 +33,14 @@ AssetShare.Profile = (function ($, ns, profileStore) {
         if(data.type === 'user') {
             // Set the profile store to enable local storage
             profileStore.setUserProfile(data);
-            //dispatch event
+            //dispatch event announcing the profile has loaded
             $('body').trigger(ns.Events.PROFILE_LOAD, {detail:data});
-           // announceProfileLoaded(data);
+        } else {
+            console.error('Could not retrieve a user profile, cart functionality disabled');
         }
         
     }).fail(function() {
-        console.error('Could not retrieve user profile, cart functionality disabled');
+        console.error('Could not retrieve a user profile, cart functionality disabled');
     });
 
 }(jQuery,
