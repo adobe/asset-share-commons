8e602fdc8300b1fb69aa7e4da1fa0c947e0f8096
David Gonzalez <davidjgonzalez@gmail.com>
2021-03-19 10:45:24 -0400

    Downloads Panel updates

--- core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/downloadspanel/DownloadsPanel.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/downloadspanel/DownloadsPanel.java
@@ -1,5 +1,7 @@
 package com.adobe.aem.commons.assetshare.components.actions.downloadspanel;
 
+import com.adobe.cq.dam.download.api.DownloadException;
+import com.adobe.cq.dam.download.api.DownloadProgress;
 import org.apache.commons.io.FileUtils;
 import org.osgi.annotation.versioning.ProviderType;
 
@@ -9,63 +11,5 @@ import java.util.List;
 
 @ProviderType
 public interface DownloadsPanel {
-
-	default List<DownloadStatus> getDownloadStatus() {
-		return Collections.EMPTY_LIST;
-	}
-
-	class DownloadStatus {
-		private final String url;
-		private final String downloadId;
-		private final int progress;
-		private final int totalCount;
-		private final String status;
-		private final long size;
-		private final String name;
-		private final Collection<String> assets;
-
-		public DownloadStatus(String name, String url, long size, String downloadId, int downloadprogress,
-				String status, int totalCount, Collection<String> assets) {
-			this.name = name;
-			this.url = url;
-			this.status = status;
-			this.size = size;
-			this.progress = downloadprogress;
-			this.downloadId = downloadId;
-			this.totalCount = totalCount;
-			this.assets = assets;
-		}
-
-		public Collection<String> getAssets() {
-			return assets;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public int getTotalCount() {
-			return totalCount;
-		}
-
-		public String getUrl() {
-			return url;
-		}
-
-		public String getDownloadId() {
-			return downloadId;
-		}
-
-		public int getProgress() {
-			return progress;
-		}
-
-		public String getStatus() {
-			return status;
-		}
-
-		public String getSize() {
-			return FileUtils.byteCountToDisplaySize(size);
-		}
-	}
+	List<DownloadProgress> getDownloads() throws DownloadException;
 }
--- core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/downloadspanel/impl/DownloadsPanelImpl.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/downloadspanel/impl/DownloadsPanelImpl.java
@@ -2,124 +2,92 @@ package com.adobe.aem.commons.assetshare.components.actions.downloadspanel.impl;
 
 import com.adobe.aem.commons.assetshare.components.actions.ActionHelper;
 import com.adobe.aem.commons.assetshare.components.actions.downloadspanel.DownloadsPanel;
-import com.adobe.aem.commons.assetshare.content.async.download.AsyncDownload;
 import com.adobe.cq.dam.download.api.DownloadArtifact;
 import com.adobe.cq.dam.download.api.DownloadException;
 import com.adobe.cq.dam.download.api.DownloadProgress;
+import com.adobe.cq.dam.download.api.DownloadService;
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
 import com.day.cq.wcm.api.WCMMode;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Required;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
-@Model(adaptables = { SlingHttpServletRequest.class }, adapters = { DownloadsPanel.class })
-public class DownloadsPanelImpl implements DownloadsPanel {
+import javax.annotation.Nonnull;
+import javax.annotation.PostConstruct;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Model(
+		adaptables = { SlingHttpServletRequest.class },
+		adapters = { DownloadsPanel.class },
+		resourceType = DownloadsPanelImpl.RESOURCE_TYPE
+)
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class DownloadsPanelImpl implements DownloadsPanel, ComponentExporter {
 
 	private static final Logger log = LoggerFactory.getLogger(DownloadsPanelImpl.class);
 	
-	private static final String LABEL_PROCESSING = "Processing";
-	private static final String LABEL_TIMESTAMP_ZIP = "timestamp.zip";
+	static final String RESOURCE_TYPE = "asset-share-commons/components/modals/downloads-panel";
 
 	@Self
 	@Required
 	private SlingHttpServletRequest request;
 
+	@SlingObject
+	@Required
+	private ResourceResolver resourceResolver;
+
 	@OSGiService
 	private ActionHelper actionHelper;
 
 	@OSGiService
-	private AsyncDownload asyncDownload;
+	private DownloadService downloadService;
 
-	private Collection<String> paths = new ArrayList<>();
-	private String[] downloadIds = null;
-	protected List<DownloadStatus> downloadStatusList = null;
-
-	@PostConstruct
-	protected void init() {
-		try {
-			String requestDLs = request.getParameter("downloadIds");
-			if (requestDLs != null) {
-				downloadIds = requestDLs.split(",");
-			}
-		} catch (Exception e) {
-			log.error("Error While processing Async download ", e);
-		}
-	}
+	private List<DownloadProgress> activeDownloads = null;
 
 	@Override
-	public List<DownloadStatus> getDownloadStatus() {
-		downloadStatusList = new ArrayList<>();
+	public List<DownloadProgress> getDownloads() throws DownloadException {
+		if (activeDownloads == null) {
+			activeDownloads = new ArrayList<>();
 
-		try {
-			if (downloadIds != null) {
-				for (String downloadId : downloadIds) {
-					DownloadStatus downloadStatus = getDownloadStatusByID(request.getResourceResolver(), downloadId);
-					downloadStatusList.add(downloadStatus);
-				}
-			} else if (!WCMMode.DISABLED.equals(WCMMode.fromRequest(request))) {
-					downloadStatusList = getPlaceholderDownload();
-			}
-		} catch (Exception e) {
-			log.error("Error while processing downloads info", e);
-		}
+			if (!WCMMode.EDIT.equals(WCMMode.fromRequest(request))) {
+				activeDownloads = StreamSupport.stream(downloadService.getDownloadIds(resourceResolver).spliterator(), false)
+						.map(id -> {
+							try {
+								return downloadService.getProgress(id, resourceResolver);
+							} catch (DownloadException e) {
+								log.warn("Unable to get async DownloadProgress for downloadId [ {} ] for user [ {} ]", id, resourceResolver.getUserID(), e);
+								return null;
+							}
+						})
+						.filter(Objects::nonNull)
+						.collect(Collectors.toList());
+			} else {
+				activeDownloads.add(new PlaceholderDownloadProgress());
+				activeDownloads.add(new PlaceholderDownloadProgress());
 
-		return Collections.unmodifiableList(downloadStatusList);
-	}
-	
-	private List<DownloadStatus> getPlaceholderDownload(){
-		downloadStatusList = new ArrayList<>();
-		DownloadStatus downloadPlaceholderStatus = getPlaceholderDownloadStatus();
-		downloadStatusList.add(downloadPlaceholderStatus);
-		
-		return downloadStatusList;
-	}
-
-	private DownloadStatus getDownloadStatusByID(ResourceResolver resourceResolver, String downloadId)
-			throws DownloadException {
-
-		DownloadProgress progress = asyncDownload.getDownloadStatus(resourceResolver, downloadId);
-		DownloadStatus downloadStatus = null;
-		DownloadArtifact downloadArtifact = getDownloadArtifact(progress);
-
-		if (downloadArtifact != null) {
-			downloadStatus = new DownloadStatus(downloadArtifact.getName(), downloadArtifact.getBinaryURI().toString(),
-					progress.getTotalSize(), downloadId, progress.getProgress(), progress.getStatus().toString(),
-					progress.getTotalCount(), downloadArtifact.getSuccesses());
-		} else {
-			downloadStatus = new DownloadStatus(LABEL_PROCESSING, "", 0, downloadId, progress.getProgress(),
-					progress.getStatus().toString(), progress.getTotalCount(), null);
-		}
-
-		return downloadStatus;
-	}
-	
-
-	public final DownloadStatus getPlaceholderDownloadStatus() {
-		DownloadStatus placeHolderDownloadStatus = null;
-		placeHolderDownloadStatus = new DownloadStatus(LABEL_TIMESTAMP_ZIP, "", 0, "", 0, "Status", 0, null);
-		return placeHolderDownloadStatus;
-	}
-
-	private DownloadArtifact getDownloadArtifact(DownloadProgress progress) {
-		DownloadArtifact returningArtifact = null;
-		// retrieve successfully generated URIs
-		for (DownloadArtifact artifact : progress.getArtifacts()) {
-			if (artifact.getBinaryURI() != null) {
-				returningArtifact = artifact;
-				break;
 			}
 		}
-		return returningArtifact;
+
+		return Collections.unmodifiableList(activeDownloads);
 	}
 
+	@Nonnull
+	@Override
+	public String getExportedType() {
+		return RESOURCE_TYPE;
+	}
 }
