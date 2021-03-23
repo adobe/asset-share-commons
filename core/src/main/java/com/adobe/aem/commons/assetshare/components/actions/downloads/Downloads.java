d3474681315926dc0e9de1305f9ccc9c79891e6f
David Gonzalez <davidjgonzalez@gmail.com>
2021-03-23 14:24:06 -0400

    #cleanup

--- core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/downloads/Downloads.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/components/actions/downloads/Downloads.java
@@ -12,7 +12,7 @@ public interface Downloads {
 	/**
 	 * This model is adapted from a SlingHttpServletRequest object that must have a RequestParameters to indicate the requested DownloadProgresses to return.
 	 *
-	 * - downloadIds = String[] of downloadIds to retrieve DownloadProgresses for
+	 * - downloadId: 1 or more request parameters of this name whose values are the allowed downloadIds to include in the result.
 	 *
 	 * @return a list of the Download Progresses the user has request and also has access to.
 	 * @throws DownloadException
