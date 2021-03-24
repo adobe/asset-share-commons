31455d820d7da7ca733b3b8a66b93ddbf5250346
David Gonzalez <davidjgonzalez@gmail.com>
2021-03-24 16:15:00 -0400

    #cleanup

--- ui.apps/src/main/content/jcr_root/apps/asset-share-commons/components/modals/downloads/clientlibs/site/js/downloads.js
+++ ui.apps/src/main/content/jcr_root/apps/asset-share-commons/components/modals/downloads/clientlibs/site/js/downloads.js
@@ -25,38 +25,6 @@ jQuery((function($, ns, semanticModal, download) {
             DOWNLOADS_MODAL_ID = "downloads-modal",
             WHEN_DOWNLOADS_UPDATED = "downloads-updated";
 
-        /**
-        * Function copied from: https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string/10420404
-        * Format bytes as human-readable text.
-        *
-        * @param bytes Number of bytes.
-        * @param si True to use metric (SI) units, aka powers of 1000. False to use
-        *           binary (IEC), aka powers of 1024.
-        * @param dp Number of decimal places to display.
-        *
-        * @return Formatted string.
-        */
-        function humanFileSize(bytes, si=false, dp=1) {
-            const thresh = si ? 1000 : 1024;
-
-            if (Math.abs(bytes) < thresh) {
-                return bytes + ' B';
-            }
-
-            const units = si
-             ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-             : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-            let u = -1;
-            const r = 10**dp;
-
-            do {
-                bytes /= thresh;
-                ++u;
-            } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
-
-            return bytes.toFixed(dp) + ' ' + units[u];
-        }
-
         function getId() {
             return DOWNLOAD_PANEL_MODAL_ID;
         }
@@ -140,11 +108,6 @@ jQuery((function($, ns, semanticModal, download) {
                         trigger: '.accordion-trigger',
                         content: '.accordion-content'
                     }
-                });
-                el.find('[data-asset-share-id="file-size"]').each(function() {
-                    $(this).text(function() {
-                        return humanFileSize(($(this).text() || 0), true, 0)
-                    });
                 });
             });
         }
