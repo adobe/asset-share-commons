2c513c4b6ee9e5371088ebaa8529bc12c42e3550
Shivani gupta <shivanigupta@shivanis-mbp-2.mynetworksettings.com>
2023-07-20 19:21:46 -0400

    Indentation fix

--- core/src/main/java/com/adobe/aem/commons/assetshare/components/predicates/impl/PropertyPredicateImpl.java
+++ core/src/main/java/com/adobe/aem/commons/assetshare/components/predicates/impl/PropertyPredicateImpl.java
@@ -19,31 +19,6 @@
 
 package com.adobe.aem.commons.assetshare.components.predicates.impl;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import javax.annotation.PostConstruct;
-import javax.inject.Named;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.models.annotations.Default;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Exporter;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Required;
-import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
 import com.adobe.aem.commons.assetshare.components.predicates.AbstractPredicate;
 import com.adobe.aem.commons.assetshare.components.predicates.PropertyPredicate;
 import com.adobe.aem.commons.assetshare.components.predicates.impl.options.JsonOptionItem;
@@ -62,62 +37,89 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Required;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 @Model(
-		adaptables = {SlingHttpServletRequest.class},
-		adapters = {PropertyPredicate.class, ComponentExporter.class},
-		resourceType = {PropertyPredicateImpl.RESOURCE_TYPE},
-		defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
-		)
+        adaptables = {SlingHttpServletRequest.class},
+        adapters = {PropertyPredicate.class, ComponentExporter.class},
+        resourceType = {PropertyPredicateImpl.RESOURCE_TYPE},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class PropertyPredicateImpl extends AbstractPredicate implements PropertyPredicate, Options {
 
-	protected static final String RESOURCE_TYPE = "asset-share-commons/components/search/property";
-	protected static final String PN_TYPE = "type";
-	protected String valueFromRequest = null;
-	protected ValueMap valuesFromRequest = null;
-	protected static final String DAM_RESOURCE_TYPE = "dam:Asset";
-	@Self
-	@Required
-	private SlingHttpServletRequest request;
+    protected static final String RESOURCE_TYPE = "asset-share-commons/components/search/property";
+    protected static final String PN_TYPE = "type";
 
-	@Self
-	@Required
-	private Options coreOptions;
+    protected String valueFromRequest = null;
+    protected ValueMap valuesFromRequest = null;
+    protected static final String DAM_RESOURCE_TYPE = "dam:Asset";
+    
+    @Self
+    @Required
+    private SlingHttpServletRequest request;
 
-	@ValueMapValue
-	private String label;
+    @Self
+    @Required
+    private Options coreOptions;
 
-	@ValueMapValue
-	private String property;
+    @ValueMapValue
+    private String label;
 
-	@ValueMapValue
-	private String operation;
+    @ValueMapValue
+    private String property;
 
-	@ValueMapValue
-	private Boolean expanded;
+    @ValueMapValue
+    private String operation;
 
-	@ValueMapValue(name = PropertyPredicateImpl.PN_TYPE)
-	private String typeString;
+    @ValueMapValue
+    private Boolean expanded;
 
-	@ValueMapValue
-	@Named("and")
-	@Default(booleanValues = false)
-	private boolean and;
+    @ValueMapValue(name = PropertyPredicateImpl.PN_TYPE)
+    private String typeString;
 
-	@ValueMapValue
+    @ValueMapValue
+    @Named("and")
+    @Default(booleanValues = false)
+    private boolean and;
+
+    @ValueMapValue
 	private String source;
 
 	@ValueMapValue
 	private String jsonSource;
+	
+    @PostConstruct
+    protected void init() {
+        initPredicate(request, coreOptions);
+    }
 
-	@PostConstruct
-	protected void init() {
-		initPredicate(request, coreOptions);
-	}
+    /* Options - Core Component Delegates */
 
-	/* Options - Core Component Delegates */
-
-	public List<OptionItem> getItems() {
+    public List<OptionItem> getItems() {
 		final ValueMap initialValues = getInitialValues();
 		final List<OptionItem> processedOptionItems = new ArrayList<>();
 		final boolean useDefaultSelected = !isParameterizedSearchRequest();
@@ -142,80 +144,80 @@ public class PropertyPredicateImpl extends AbstractPredicate implements Property
 		return processedOptionItems;
 	}
 
-	public Type getType() {
-		return coreOptions.getType();
-	}
+    public Type getType() {
+        return coreOptions.getType();
+    }
 
-	/* Property Predicate Specific */
+    /* Property Predicate Specific */
 
-	public String getSubType() {
-		//support variation of Checkboxes
-		return typeString;
-	}
+    public String getSubType() {
+        //support variation of Checkboxes
+        return typeString;
+    }
 
-	@Override
-	public String getProperty() {
-		return property;
-	}
+    @Override
+    public String getProperty() {
+        return property;
+    }
 
-	@Override
-	public String getValuesKey() {
-		return PropertyValuesPredicateEvaluator.VALUES;
-	}
+    @Override
+    public String getValuesKey() {
+        return PropertyValuesPredicateEvaluator.VALUES;
+    }
 
-	@Override
-	public boolean hasOperation() {
-		return StringUtils.isNotBlank(getOperation());
-	}
+    @Override
+    public boolean hasOperation() {
+        return StringUtils.isNotBlank(getOperation());
+    }
 
-	@Override
-	public String getOperation() {
-		return operation;
-	}
+    @Override
+    public String getOperation() {
+        return operation;
+    }
 
-	public boolean hasAnd() {
-		return and;
-	}
+    public boolean hasAnd() {
+        return and;
+    }
 
-	public Boolean getAnd() {
-		return and;
-	}
+    public Boolean getAnd() {
+        return and;
+    }
 
-	@Override
-	public String getName() {
-		return PropertyValuesPredicateEvaluator.PREDICATE_NAME;
-	}
+    @Override
+    public String getName() {
+        return PropertyValuesPredicateEvaluator.PREDICATE_NAME;
+    }
 
-	@Override
-	public boolean isReady() {
-		return coreOptions.getItems().size() > 0;
-	}
+    @Override
+    public boolean isReady() {
+        return coreOptions.getItems().size() > 0;
+    }
 
-	@Override
-	public String getInitialValue() {
-		if (valueFromRequest == null) {
-			valueFromRequest = PredicateUtil.getInitialValue(request, this, PropertyValuesPredicateEvaluator.VALUES);
-		}
+    @Override
+    public String getInitialValue() {
+        if (valueFromRequest == null) {
+            valueFromRequest = PredicateUtil.getInitialValue(request, this, PropertyValuesPredicateEvaluator.VALUES);
+        }
 
-		return valueFromRequest;
-	}
+        return valueFromRequest;
+    }
 
-	@Override
-	public ValueMap getInitialValues() {
-		if (valuesFromRequest == null) {
-			valuesFromRequest = PredicateUtil.getInitialValues(request, this, PropertyValuesPredicateEvaluator.VALUES);
-		}
+    @Override
+    public ValueMap getInitialValues() {
+        if (valuesFromRequest == null) {
+            valuesFromRequest = PredicateUtil.getInitialValues(request, this, PropertyValuesPredicateEvaluator.VALUES);
+        }
 
-		return valuesFromRequest;
-	}
+        return valuesFromRequest;
+    }
 
-	@Nonnull
-	@Override
-	public String getExportedType() {
-		return RESOURCE_TYPE;
-	}
-
-	private List<JsonOption> getJsonItems(){
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
+    }
+    
+    private List<JsonOption> getJsonItems(){
 		List<JsonOption> options = new ArrayList<>();
 		// Get option from json type asset and inject via jsonSource filepath
 		@Nullable
